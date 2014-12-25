12af333d9a6f422c21d0a49eeea0df0b1857edaa
atsumo <moriyama_atsushi@cyberagent.co.jp>
2014-12-25 15:03:41 +0900

    add test code js

---
+++ test/test.converter.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var cli = require('../lib/cli');
+var program = require('commander');
+//TODO test
+describe('#cli', function() {
+  it('cli test', function(done) {
+    //assert.
+    done();
+  });
+});
